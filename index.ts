de0e4894e37cf494d05977c3c8d8c80f4ced278d
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-09-11 19:19:16 -0300

    feat: create a simple http server

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: process.env.PORT || 3000,
+  fetch() {
+    return Response.json({
+      message: "Bun!"
+    });
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port}!`);
